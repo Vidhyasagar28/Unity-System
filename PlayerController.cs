f7342465e945054d240e484ed937d0cf0e01c557
Vidhya Sagar <95131700+Vidhyasagar28@users.noreply.github.com>
2025-08-16 18:56:09 +0530

    Add files via upload

--- PlayerController.cs
+++ PlayerController.cs
@@ -10,7 +10,13 @@ public class PlayerController : MonoBehaviour
 
     [Header("Movement Configs")]
     [SerializeField] Vector3 movement;
+    [SerializeField] float CurrentSpeed;
     [SerializeField] float WalkSpeed;
+    [SerializeField] float SprintSpeed;
+    [SerializeField] bool isSprinting;
+    bool isCrouch;
+    float smoothenAngle;
+    
 
     [Header("Touch Grass?")]
     [SerializeField] bool IsPlayerGrounded;
@@ -25,19 +31,23 @@ public class PlayerController : MonoBehaviour
     [SerializeField] float jumpHeight;
     [SerializeField] float applyGravity = 0;
 
+    Transform cam;
     // Start is called before the first frame update
     void Start()
     {
         character = GetComponent<CharacterController>();
+        cam = Camera.main.transform;
+        InputManager._instance.OnCrouching += SetCrouch;
     }
 
     // Update is called once per frame
     void Update()
     {
         #region Get Inputs
-        Vector2 moveInput = InputManager._instance.MoveInput;
+        Vector3 horizontal = InputManager._instance.GetMovementInput() * CurrentSpeed;
+        horizontal = horizontal.z * cam.forward.normalized + horizontal.x * cam.right.normalized;
         IsJumping = InputManager._instance.isJumping;
-
+        isSprinting = InputManager._instance.isSprinting;
         #endregion
 
         #region Ground & Gravity 
@@ -51,24 +61,45 @@ public class PlayerController : MonoBehaviour
         {
             applyGravity = Mathf.Sqrt(2f * gravity * jumpHeight);
         }
-
+        applyGravity -= gravity * Time.deltaTime;
         #endregion
 
-        
-        
-        applyGravity -= gravity * Time.deltaTime;
-
-        Vector3 horizontal = (transform.right * moveInput.x + transform.forward * moveInput.y) * WalkSpeed;
         Vector3 vertical = new Vector3(0f, applyGravity, 0f);
 
-        movement = horizontal + vertical;        
+        movement = horizontal + vertical;
 
+        SetMovementSpeed();
+        
+        if (horizontal.sqrMagnitude > 0.01f) {
+            float rotateAngle = Mathf.Atan2(movement.x, movement.z) * Mathf.Rad2Deg;
+            float smoothAngleTurn = Mathf.SmoothDampAngle(transform.eulerAngles.y, rotateAngle, ref smoothenAngle, 0.1f);
+            transform.rotation =  Quaternion.Euler(0f, smoothAngleTurn, 0f);      
+        }
         character.Move(movement * Time.deltaTime);
     }
 
     void IsGrounded()
     {
         IsPlayerGrounded = Physics.CheckSphere(feet.position, groundCheckRadius, groundLayer);
+    }
+    void SetMovementSpeed()
+    {
+        if (isSprinting)
+            CurrentSpeed = SprintSpeed;
+        else
+            CurrentSpeed = WalkSpeed;
+    }
+    void SetCrouch()
+    {
+        isCrouch = !isCrouch;
+        if (isCrouch)
+        {
+            character.height /= 2;
+        }
+        else
+        {
+            character.height = 1.7f;
+        }
     }
     private void OnDrawGizmosSelected()
     {
